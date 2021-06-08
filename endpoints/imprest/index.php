4a28f2f03300f518dda8c028cf1c30fb49225619
Joseph Scoggins <borninthecaul@gmail.com>
2021-06-07 21:32:51 -0700

    successful plpml compile

--- endpoints/imprest/index.php
+++ endpoints/imprest/index.php
@@ -1,17 +1,17 @@
 {
-  "title" : "imprest",
-  "table" : [
-    ["shell","div","geo-block-inner","ogGeo",""],
-    ["shell_script","script","application/ld+json","",""],
-    ["shell_map","div","","og-geo-map",""],
-    ["shell_cities","div","gb-cities","",""],
-    ["cities_title","div","gb-title","",""],
-    ["title_header","h2","","","California in Vancouver WA"],
-    ["cities_subheader","p","geo-subhead","","Commercial and Residential Landscaping Services in The Greater Vancouver Area"],
-    ["cities_geolineTop","h3","gb-cities-col","","Vancouver | Camas | Washougal | Felida | Salmon Creek | Mount Vista | Ridgefield"],
-    ["cities_geolineBottom","h3","gb-cities-col","",""],
-    ["shell_buttons","div","gb-buttons","",""],
-    ["buttons_linkTop","a","","https://boulderfallsinc.com/reviews/","Our Reviews"],
-    ["buttons_linkBottom","a","","https://boulderfallsinc.com/estimate/","Free Estimate"]
-  ]
+  "title" : "imprest test pattern",
+  "nest" : [
+["shell","div#og-geo.geo-block-inner{}()"],
+["shell_script","script{type:application/javascript;src:benandjerrys.js}()"],
+["shell_map","div#og-geo-map.geo-block-innermost{}()"],
+["shell_cities","div#gb-cities.geo-block-innermost{}()"],
+["cities_title","div#gb-title.geo-block-innermost{}()"],
+["title_header","h2{}(California in Vancouver WA)"],
+["cities_subheader","p#geo-subhead.geoline{}(The Best Ideal Copy You Can Copy)"],
+["cities_geolinetop","h3#gb-cities-top.gb-cities.citiesline{}(Vancouver | Camas | Washougal | Felida | Salmon Creek | Mount Vista | Ridgefield)"],
+["cities_geolinebottom","h3#gb-cities-bottom.gb-cities.citiesline{}(Lebiski | Cornfoot)"],
+["shell_buttons","div#gb-buttons{}()"],
+["buttons_linktop","a#linktop.buttonlink{href:https%3A//goggles.com}(My Spaghetti Sauce)"],
+["buttons_linkbottom","a#linkbottom.buttonlink{href:https%3A//sniggles.com}(My Strawberry Sniggles)"]
+]
 }
